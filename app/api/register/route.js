c40e3fd8d5e6e1341c82f579e6ef9c3ffde4876b
Fathismael93 <fathismael@gmail.com>
2025-06-17 14:20:16 +0300

    implemented api register

--- app/api/register/route.js
+++ app/api/register/route.js
@@ -2,11 +2,24 @@ import dbConnect from "@/backend/config/dbConnect";
 import User from "@/backend/models/user";
 
 export async function POST(req) {
-  await dbConnect()
-    .then(() => {
-      console.log("Database connected successfully");
-    })
-    .catch((error) => {
-      console.error(`Database connection failed: ${error.message}`);
-    });
+  await dbConnect();
+
+  const { email, password } = await req.json();
+  if (!email || !password) {
+    return new Response("Email and password are required", { status: 400 });
+  }
+
+  try {
+    const existingUser = await User.findByEmail(email);
+    if (existingUser) {
+      return new Response("User already exists", { status: 409 });
+    }
+
+    const newUser = new User({ email, password });
+    await newUser.save();
+    return new Response("User registered successfully", { status: 201 });
+  } catch (error) {
+    console.error("Error registering user:", error);
+    return new Response("Internal server error", { status: 500 });
+  }
 }
